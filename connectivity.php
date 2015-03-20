d0c0467b2c98a4223645566927d57fdf746b0c4e
sujud93 <sujud.alkhawaldah@gmail.com>
2015-03-20 18:04:36 +0200

    Update connectivity.php

--- connectivity.php
+++ connectivity.php
@@ -1,7 +1,7 @@
 <?php define('DB_HOST', 'localhost'); 
       define('DB_NAME', 'users'); 
-	  //define('DB_USER','root'); 
-	  //define('DB_PASSWORD',''); 
+      define('DB_USER','root'); 
+      define('DB_PASSWORD',''); 
 	  $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error()); 
 	  $db=mysql_select_db(users,$con) or die("Failed to connect to MySQL: " . mysql_error()); 
 	  /* $ID = $_POST['user'];
